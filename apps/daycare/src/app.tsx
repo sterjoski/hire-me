d59c2780dc1c4484cf8f149f030910392cb7e4fc
Vladimir Šterjoski <vladimir.sterjoski@leanpay.com>
2024-06-05 22:30:53 +0200

    feat: setup react-query

--- apps/daycare/src/app.tsx
+++ apps/daycare/src/app.tsx
@@ -1,5 +1,18 @@
-function App() {
-    return <></>
-}
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { Daycare } from './app/daycare/daycare'
 
-export default App
+export const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            refetchOnWindowFocus: false,
+        },
+    },
+})
+
+export const App = () => {
+    return (
+        <QueryClientProvider client={queryClient}>
+            <Daycare />
+        </QueryClientProvider>
+    )
+}
