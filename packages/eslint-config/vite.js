989ad3ebc7f8bd1f83d76d1106cec430d18c1799
Vladimir Šterjoski <vladimir.sterjoski@leanpay.com>
2024-06-05 21:33:00 +0200

    chore: remove unused code

--- packages/eslint-config/vite.js
+++ packages/eslint-config/vite.js
@@ -1,16 +1,7 @@
-const { resolve } = require('node:path')
-
-const project = resolve(process.cwd(), 'tsconfig.json')
-
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
     env: { browser: true, es2020: true },
-    extends: [
-        'eslint:recommended',
-        // 'plugin:react-hooks/recommended',
-        // 'plugin:react/jsx-runtime',
-        'prettier',
-    ],
+    extends: ['eslint:recommended', 'prettier'],
     ignorePatterns: ['dist', '.eslintrc.cjs'],
     parser: '@typescript-eslint/parser',
     plugins: ['react-refresh'],
